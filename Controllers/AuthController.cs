5b968655e6136915e884e0b187da78013caa87df
Đăng Huy <99796088+Emoughl@users.noreply.github.com>
2022-06-17 06:01:11 +0700

    update-days1

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MangaStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,17 @@ using System.Web.Mvc;
 
 namespace MangaStore.Controllers
 {
-    public class AuthController : Controller
+    public class AuthController : BaseController
     {
         // GET: Auth
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Theloai()
+        {
+            var theloai = from tl in dataContext.THELOAIs select tl;
+            return PartialView(theloai);
         }
     }
 }
---
+++ Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using MangaStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MangaStore.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected DataMangaDataContext dataContext = new DataMangaDataContext();
+
+        public BaseController()
+        {
+            ViewBag.DataContext = dataContext;
+            ViewBag.TheLoai = dataContext.THELOAIs;
+        }
+    }
+}
